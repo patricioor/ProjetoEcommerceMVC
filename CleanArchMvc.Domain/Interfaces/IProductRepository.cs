15f0e76f53fd149037aff53af56e6dc5ffeb885c
Patricio Osterno Rios <patricio.osterno1@gmail.com>
2023-04-17 19:15:45 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    internal interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> GetProductCategoryAsync(int? id);
+
+        Task<Category> CreateAsync(Product product);
+
+        Task<Category> UpdateAsync(Product product);
+
+        Task<Category> RemoveAsync(Product product);
+    }
+}
+}
---
+++ CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanArchMvc.Domain.Validation
+{
+    internal class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string error) : base(error) { }
+
+        public static void When(bool HasError, string error)
+        {
+            if(HasError) throw new DomainExceptionValidation(error);
+        }
+    }
+}
